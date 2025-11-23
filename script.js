bc62f3de93533f2de3e2c7477f69d8aad9f0cb0c
itschool <itschool@tou.edu.kz>
2025-11-23 11:53:44 +0500

    урок 23/11/25 кнопка скрыть

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 //выводит окошко на экран
-// alert('321')
+// alert('321');
+
+//тольо раскрыте текста
+//document.querySelector('.info__link').addEventListener('click',function () {
+//    this.remove();
+//    document.querySelector('.info__text').classList.add('info__text--active')
+//});
+
+//let buttonText;
+//buttonText = 1;
+//buttonText = "256"
+
+//let buttonText
+//let a = 5;
+//if (a == 5) {
+//    alert('Pravda')
+//}
+//if (a !=5) {
+//    alert('lozh')
+//}
+
+/*let buttonText
+let a = 5;
+if (a == 5) {
+    alert('Pravda')
+} else {
+   alert(lozh)
+}*/
+
+/*document.querySelector('.info__link').addEventListener('click',function () {
+    document.querySelector('.info__text').classList.toggle('info__text--active')
+    buttonText = this.textContent;
+    this.textContent = "Скрыть текст";
+});*/
+
+
+let a = "Скрыть текст"
+let b
 
 document.querySelector('.info__link').addEventListener('click',function () {
-    this.remove();
-    document.querySelector('.info__text').classList.add('info__text--active')
-});
+    document.querySelector('.info__text').classList.toggle('info__text--active')
+    b = a
+    a = this.textContent
+    this.textContent = b
+    })
