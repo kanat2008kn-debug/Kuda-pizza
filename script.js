46a560dcd096d69d92d4ac898ceb85eebafda749
Kanat <kanat2008.kn@gmail.com>
2025-11-30 01:08:49 +0500

    razbor doma

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ if (a == 5) {
 });*/
 
 
-let a = "Скрыть текст"
-let b
+let buttonText;
+let buttonText2 = "Скрыть текст";
 
-document.querySelector('.info__link').addEventListener('click',function () {
-    document.querySelector('.info__text').classList.toggle('info__text--active')
-    b = a
-    a = this.textContent
-    this.textContent = b
-    })
+document.querySelector('.info__link').addEventListener('click', function () {
+    document.querySelector('.info__text').classList.toggle('info__text--active');
+    buttonText = this.textContent;
+    this.textContent = buttonText2;
+    buttonText2 =  buttonText;
+})
